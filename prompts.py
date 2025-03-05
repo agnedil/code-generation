dd127680d034ab829bcf9d596549647b3f585218
Andrew <andrew@Andrews-MacBook-Pro.local>
2025-03-05 02:49:31 -0800

    Next Version

---
+++ prompts.py
@@ -0,0 +1,99 @@
+##### HUMAN EVAL AND BIG CODE BENCH PROMPTS #####
+
+complete_code_prompt_basic = 'Complete the following Python code:\n{}'
+
+complete_code_prompt = '''
+**Role**
+You are a Python software programmer.
+
+**Instructions**
+As a programmer, you are required to complete the following Python code.
+
+**Code Formatting**
+Output the completed code in the following format:
+```python
+[Code]
+```
+
+**Code To Be Completed**
+{}
+'''
+
+complete_code_prompt_full = '''
+**Role**
+You are a Python software programmer.
+
+**Instructions**
+1. As a programmer, you are required to complete the code for the function described below.
+2. Make sure you understand the function description, complete the code and merge the function description with its completion using the correct Python syntax and proper indentation.
+3. Your output should be with no example usage and no test cases - stop generating immediately after the return statement or the final line of the function.
+4. Output only the complete, runnable Python code with no phrases like "Completion:" or "Here is a completion" and no other headings.
+5. Your output should be with no explanatory text and no other irrelevant content - it should be directly runnable by a Python interpreter without errors.
+
+**Code Formatting**
+Output the completed code in the following format:
+```python
+[Code]
+```
+
+**Function Description**
+{}
+'''
+
+##### MBPP AND LBPP PROMPTS #####
+
+complete_task_prompt_basic = '''
+Complete the following task:
+{}.
+
+Your code should satisfy these tests:
+{}
+'''
+
+complete_task_prompt = '''
+**Role**
+You are a Python software programmer.
+
+**Instructions**
+As a programmer, you are required to complete the following task:
+{}
+
+**Code Formatting**
+Output your code in the following format:
+```python
+[Code]
+```
+
+Your ouput code should satisfy these tests:
+{}
+'''
+
+
+complete_task_prompt_full = '''
+**Role**
+You are a Python software programmer.
+
+**Instructions**:
+1. As a programmer, you are required to complete the task described below.
+2. Make sure you understand the task and solve it using the correct Python syntax and proper indentation.
+3. Your output should be with no example usage and no test cases - stop generating immediately after the return statement or the final line of the function.
+4. Output only the complete, runnable Python code with no phrases like "Solution:" or "Here is a solution" and no other headings.
+5. Your output should be with no explanatory text and no other irrelevant content - it should be directly runnable by a Python interpreter without errors.
+
+**Code Formatting**
+Output your code in the following format:
+```python
+[Code]
+```
+
+**Task Description**
+{}
+
+Your ouput code should satisfy these tests:
+{}
+'''
+
+
+
+if __name__ == "__main__":
+    print(human_eval_completion_prompt + 'Next line')
