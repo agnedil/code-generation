d4d9be8955fea99be3b11393228cac456be51389
Andrew <andrew@Andrews-MacBook-Pro.local>
2025-04-26 05:21:59 -0700

    Next version

--- notebooks/helpers.py
+++ notebooks/helpers.py
@@ -1,9 +1,84 @@
+
 import re
 import sys
 import os
 import gzip
 import json
-from typing import Iterable, Dict
+import torch
+from typing import Dict, Iterable, Any
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+# max sequence length
+MAX_LEN = 2048
+
+def get_tokenizer(model_name: str, ACCESS_TOKEN: str | None = None) -> Any:
+
+    if ACCESS_TOKEN:
+        tokenizer = AutoTokenizer.from_pretrained( model_name,
+                                                   trust_remote_code=True,
+                                                   token=ACCESS_TOKEN, )
+    else:
+        tokenizer = AutoTokenizer.from_pretrained( model_name,
+                                                   trust_remote_code=True, )
+    tokenizer.add_special_tokens({"pad_token": "<pad>"})
+    tokenizer.pad_token = '<pad>'
+    return tokenizer
+
+
+def get_model(model_name: str, torch_dtype = torch.float32, ACCESS_TOKEN: str|None = None) -> Any:
+    ''' Return model depending on the model name.
+        Note: * float16 - more precise but has narrow range,
+              * bfloat16 less precision, but dynamic range closer to float32 -> useful for DL
+              * float32 doesn't have a bfloat32 equivalent as it is already sufficient.
+              * Solar 'auto' gets me bfloat16, when I ask for float32 - CUDA out of memory!
+              * model.eval() sets model to evaluation mode, which disables dropout,
+                sets batch normalization layers to use their running statistics,
+                and generally ensures more deterministic behavior
+
+              TODO: test speed vs. accuracy for bloat16 vs. float32 (two separate runs)
+    '''
+    if ACCESS_TOKEN:
+        model = AutoModelForCausalLM.from_pretrained( model_name,
+                                                      device_map='cuda',
+                                                      trust_remote_code=True,
+                                                      torch_dtype=torch_dtype,        # bfloat16, float32, 'auto'
+                                                      token = ACCESS_TOKEN, )
+    else:
+        model = AutoModelForCausalLM.from_pretrained( model_name,
+                                                      device_map='cuda',
+                                                      trust_remote_code=True,                                                      
+                                                      torch_dtype=torch_dtype,        # bfloat16, float32, 'auto'
+                                                     )
+    model.eval()
+    return model
+
+
+def generate_response(prompt, tokenizer, model, temperature=1.0, top_p=1.0):
+    ''' Tokenize input and make one inference call '''
+    messages=[
+      { 'role': 'user', 'content': prompt }
+    ]
+    inputs = tokenizer.apply_chat_template(
+        messages,
+        padding=True,
+        truncation=True,
+        max_length=MAX_LEN,
+        add_generation_prompt=True,
+        return_tensors="pt",
+    ).to(model.device)
+    outputs = model.generate(
+        inputs,
+        max_new_tokens=MAX_LEN,
+        do_sample=False,
+        temperature=temperature,
+        top_p=top_p,
+        #top_k=50,
+        num_return_sequences=1,
+        pad_token_id=tokenizer.eos_token_id,
+        eos_token_id=tokenizer.eos_token_id,
+    )
+    res = tokenizer.decode( outputs[0][len(inputs[0]):], skip_special_tokens=True, )
+    return res
 
 
 def clean_code(code: str, signature: str=None) -> str:
@@ -13,32 +88,82 @@ def clean_code(code: str, signature: str=None) -> str:
           Other opening code fences include: "```python3", "```Python", or "```Python3".
         * Remove any lines w/test cases starting with 'assert'
         * Remove any lines w/test cases starting with "print(function_name(".
+        * Add func header if missing
+        * Add import statements if missing, including 'from typing import *'
+        * Remove the 'main' body
+        * Remove some irrelevant lines
     '''
     # edge cases
     if not isinstance(code, str):
         print('WARNING. Not a string passed for evaluation of generated code - returning an empty string')
         return ''
     if not code:
-        return code    
-    code    = get_code_in_fences(code)                     # parse code out of code fences    
+        return code
+    
+    # implement steps to clean code
+    code    = get_code_in_fences(code)                     # parse code out of code fences
+    code    = split_code_on_main(code)                     # keep functions, remove main body
     code    = remove_assert_statements(code)               # remove lines starting with 'assert'    
     code    = remove_print_function_statements(code)       # remove lines starting with "print(function_name("
-    is_func = is_function(code)
-    if signature and not is_func:                          # add func signature if missing
-        code = join_properly(signature, code)
-        is_func = True
-    if is_func and 'from typing import' not in code:
-        code = 'from typing import *\n' + code
-    return code
+    code    = remove_irrelevant_lines(code)                # remove specific irrelevant lines
+    if signature:
+        is_func = is_function(code)
+        if not is_func:                                    # add func signature if missing
+            code = join_properly(signature, code)
+        else:
+            code = merge_imports_with_code(signature, code)    # add all original import statements
+    code = add_typing_import(code)                         # add from typing import * (if missing)
+    return code.strip()
+
+
+def clean_code_light(code: str, signature: str=None) -> str:
+    '''
+    Clean code by:
+        * Extracting only code between "```python" and "```" code fences.
+          Other opening code fences include: "```python3", "```Python", or "```Python3".
+        * Add func header if missing
+        * Add import statements if missing, including 'from typing import *'
+        * Remove the 'main' body
+        * Remove some irrelevant lines
+
+    Compared with clean_code() above, this function doesn't do this:
+        * Remove any lines w/test cases starting with 'assert'
+        * Remove any lines w/test cases starting with "print(function_name(".
+        
+    Reason: supposedly, these tests don't break the code execution while they may break it after
+            an attempt to remove them, because they may span more than one line - TBD.
+    '''
+    # edge cases
+    if not isinstance(code, str):
+        print('WARNING. Not a string passed for evaluation of generated code - returning an empty string')
+        return ''
+    if not code:
+        return code
+    
+    # implement steps to clean code
+    code    = get_code_in_fences(code)                     # parse code out of code fences
+    code    = split_code_on_main(code)                     # keep functions, remove main body
+    #code    = remove_assert_statements(code)              # remove lines starting with 'assert'    
+    #code    = remove_print_function_statements(code)      # remove lines starting with "print(function_name("
+    code    = remove_irrelevant_lines(code)                # remove specific irrelevant lines
+    if signature:
+        is_func = is_function(code)
+        if not is_func:                                    # add func signature if missing
+            code = join_properly(signature, code)
+        else:
+            code = merge_imports_with_code(signature, code)    # add all original import statements
+    code = add_typing_import(code)                         # add from typing import * (if missing)
+    return code.strip()
 
 
 def join_properly(signature: str, body: str) -> str:
     """
     Join function signature and body using proper indentation.
-        :param signature: function signarure, something like 'def my_func(x):'.
+        :param signature: function signature, something like 'def my_func(x):'.
         :param body: function body.
         :return: A single string that combines the signature and the (re)indented body.
     """
+    signature = signature.strip()
     lines = body.splitlines()
 
     # Strip empty lines
@@ -63,6 +188,13 @@ def join_properly(signature: str, body: str) -> str:
 
 def get_code_in_fences(code: str) -> str:
     ''' Parse code from 4 different variations of code fences '''
+    # edge cases
+    if not isinstance(code, str):
+        print('WARNING. Not a string passed for evaluation of generated code - returning an empty string')
+        return ''
+    if not code:
+        return code
+    
     fence_pattern = re.compile(r"```[ \t]*(?P<lang>python(?:3)?)", re.IGNORECASE)    # shorter, simpler
     #fence_pattern = re.compile(r"```[ \t]*(?P<lang>python(?:3)?)[ \t]*(?=$|\r?\n)", re.IGNORECASE)
     match = fence_pattern.search(code)    
@@ -75,10 +207,8 @@ def get_code_in_fences(code: str) -> str:
 
 def remove_assert_statements(code: str) -> str:
     ''' Remove lines starting with "assert" '''
-    if not 'assert' in code: return code.strip()
-    return '\n'.join(
-                line for line in code.splitlines() if not line.lstrip().startswith('assert')
-            ).strip()
+    if not 'assert' in code: return code
+    return '\n'.join( line for line in code.splitlines() if not line.lstrip().startswith('assert') )
 
 
 def remove_print_function_statements(code: str) -> str:
@@ -86,14 +216,12 @@ def remove_print_function_statements(code: str) -> str:
     
     # find function_name
     match = re.search(r'def\s+(\w+)\(', code)
-    if not match: return code.strip()
+    if not match: return code
     func_name     = match.group(1)
     print_pattern = re.compile(r'^\s*print\(\s*' + re.escape(func_name) + r'\(')  # matching "print(function_name("
 
     # if function_name found, remove lines starting with "print(function_name("
-    return '\n'.join(
-                line for line in code.splitlines() if not print_pattern.match(line)
-            ).strip()
+    return '\n'.join( line for line in code.splitlines() if not print_pattern.match(line) )
 
 
 def is_function(text: str) -> bool:
@@ -105,19 +233,84 @@ def is_function(text: str) -> bool:
     return False
 
 
+def add_typing_import(code):
+    ''' If needed add line: from typing import *'''
+    typing_import = 'from typing import *'
+    if typing_import[:-2] not in code:
+        if code.strip().startswith('def'):
+            code = typing_import + '\n\n' + code
+        else:
+            code = typing_import + '\n' + code
+    return code
+
+
 def get_import_statements(code: str) -> str:
-    ''' Keeps only lines starting w/'import' or 'from ' '''
+    ''' Keeps only lines starting w/'import' or 'from ' that occur before function definition'''
     lines = code.splitlines()
     filtered_lines = []
     for line in lines:
         stripped_line = line.strip()
+        if stripped_line.startswith('def '):          # avoid finding lines starting with from in docstring
+            break
         if stripped_line.startswith('import ') or stripped_line.startswith('from '):
-            filtered_lines.append(line)
-    res = '\n'.join(filtered_lines) + '\n'
-    s = 'from typing import *'
-    if 'from typing import' not in res:
-        res = s + '\n' + res
-    return res
+            filtered_lines.append( stripped_line )    # these are imports before the function definition => must be stripped
+    return '\n'.join(filtered_lines).strip()
+
+
+def merge_imports_with_code(signature: str, code: str,) -> str:
+    '''
+      1. Extract import statements from the signature.
+      2. Add them to code.
+      Simplified version. Earlier version of this function tried to extract any import statements
+      from code too to combine the unique import statements from both signature and code.
+      But it turned out that this intrusive method is prone to errors.
+    '''
+    # need not signature here in case signature=None
+    if not signature or not signature.strip():
+        return code
+        
+    # Step 1: Extract imports from signature
+    signature_imports = get_import_statements(signature)
+    if not signature_imports.strip():
+        return code
+    
+    # Step 2: Add them to code
+    code = signature_imports + '\n' + code
+    return code.strip()
+
+
+def split_code_on_main(code: str) -> str:
+    '''
+    Keep only the part of code that comes before the first
+    occurrence of 'if __name__ == "__main__":'
+    '''
+    if not code:
+        return code
+    markers = ['if __name__ == "__main__":', "if __name__ == '__main__':"]
+    positions = [code.find(marker) for marker in markers]
+    valid_positions = [pos for pos in positions if pos != -1]
+    if valid_positions:
+        first_marker_index = min(valid_positions)
+        return code[:first_marker_index].strip()
+    return code
+
+
+def remove_irrelevant_lines(code: str) -> str:
+    '''
+    Remove lines that are strictly one of the following (after strip()):
+      - ``` 
+      - [code]
+      - [/code]
+    '''
+    if not code:
+        return code
+    filtered_lines = []
+    for line in code.splitlines():
+        stripped_line = line.strip()
+        if stripped_line in ("```", "[code]", "[/code]"):
+            continue
+        filtered_lines.append(line)
+    return '\n'.join(filtered_lines)
 
 
 def write_jsonl(filename: str, data: Iterable[Dict], append: bool = False) -> None:
@@ -210,6 +403,7 @@ if __name__ == "__main__":
     for s in [s1, s2]:
         print(is_function(s))
         
+    
     s1 = '''
     Here is an improved solution:
     ``` Python3
@@ -242,9 +436,27 @@ if __name__ == "__main__":
     print(check_value({'Cierra Vega': 12, 'Alden Cantrell': 12, 'Kierra Gentry': 12, 'Pierre Cox': 12},12))
     print(check_value({'Cierra Vega': 12, 'Alden Cantrell': 12, 'Kierra Gentry': 12, 'Pierre Cox': 12},5))
     """
+
+    s4 = '''
+    Here is an improved solution:
+    ```Python3
+    def get_res(s):
+        return s.upper()
+    ```
+    '''
+
+    signature = '''import collections
+    import random
+    import string
+
+    def task_func(length=100):
+        """ Function to perform a task """
+    '''
     
-    for s in [s1, s2, s3]:
+    for idx, s in enumerate([s1, s2, s3]):
+        print(f'Iteration {idx+1}\n')
         print(clean_code(s), '\n')
+        print(clean_code(s, signature), '\n')
         
     code_with_imports = '''import collections
     import random
@@ -255,4 +467,19 @@ if __name__ == "__main__":
     '''
     print( get_import_statements(code_with_imports) )
 
+    code1 = '''import time
+    import datetime
 
+    def task_func(length=100):
+        length *= 100
+        return 5 + length
+    '''
+
+    code2 = '''def task_func(length=100):
+        length *= 100
+        return 5 + length
+    '''
+
+    for code in [code1, code2]:
+        print(merge_imports_with_code(signature, code), '\n')
+        print(clean_code(code, signature), '\n')
--- notebooks/prompts.py
+++ notebooks/prompts.py
@@ -1,42 +1,45 @@
 ##### HUMAN EVAL AND BIG CODE BENCH PROMPTS #####
 
-complete_code_prompt_basic = 'Complete the following Python code:\n{}'
+complete_code_prompt_basic = '''
+Complete the following Python code:
+{}
+'''
 
 complete_code_prompt = '''
-**Role**
-You are a Python software programmer.
-
-**Instructions**
-As a programmer, you are required to complete the following Python code.
-
-**Code Format**
-Output the completed code in the following format:
+1. Act as an experienced Python software developer and complete the code provided below.
+2. Enclose your output code in the following code fences:
 ```python
-<code>
+[code]
 ```
-
-**Code To Be Completed**
+3. Complete the following Python code:
 {}
 '''
 
 complete_code_prompt_full = '''
-**Role**
-You are a Python software programmer.
-
-**Instructions**
-1. As a programmer, you are required to complete the code for the function described below.
-2. Make sure you understand the function description, complete the code and merge the function description with its completion using the correct Python syntax and proper indentation.
-3. Your output should be with no example usage and no test cases - stop generating immediately after the return statement or the final line of the function.
-4. Output only the complete, runnable Python code with no phrases like "Completion:" or "Here is a completion" and no other headings.
-5. Your output should be with no explanatory text and no other irrelevant content - it should be directly runnable by a Python interpreter without errors.
-
-**Code Format**
-Output the completed code in the following format:
+1. Act as an experienced Python software developer and complete the starter code provided below.
+2. Understand the task described in the starter code, generate the completion, and integrate the completion with the starter code using Python's correct syntax and proper indentation.
+3. Stop generating immediately after the return statement or the final line of the function.
+4. Exclude any non-code content. For example, exclude explanatory text, exclude example usage, exclude test cases, exclude phrases like "Completion:" or "Here is a completion", exclude any other headings, and exclude anything that can be considered as non-executable Python code.
+5. Your final output must be only code that can be executed directly by a Python interpreter without errors.
+6. Enclose your final output code in the following code fences:
 ```python
-<code>
+[code]
 ```
+7. Using the above instructions, complete the following Python code:
+{}
+'''
 
-**Function Description**
+complete_code_prompt_full_OLD = '''
+1. Act as an experienced Python software developer and complete the starter code provided below.
+2. Make sure you understand the starter code, generate its completion, and integrate your completion with the provided starter code using Python's correct syntax and proper indentation.
+3. Your output must be only code and it must not contain any irrelevant non-code content: it should be with no explanatory text, no example usage, no test cases, no phrases like "Completion:" or "Here is a completion", and no other headings.
+4. Therefore, stop generating immediately after the return statement or the final line of the function.
+5. Your final output code must be directly runnable by a Python interpreter without errors.
+6. Enclose your final runnable output code in the following code fences:
+```python
+[code]
+```
+7. Using all of the above instructions, complete the following Python code:
 {}
 '''
 
@@ -46,54 +49,102 @@ complete_task_prompt_basic = '''
 Complete the following task:
 {}.
 
-Your code should satisfy these tests:
+Your output code must satisfy these tests:
 {}
 '''
 
 complete_task_prompt = '''
-**Role**
-You are a Python software programmer.
-
-**Instructions**
-As a programmer, you are required to complete the following task:
+1. Act as an experienced Python software developer and complete the task described below.
+2. Enclose your output code in the following code fences:
+```python
+[code]
+```
+3. Complete the following task:
 {}
 
-**Code Format**
-Output your code in the following format:
-```python
-<code>
-```
-
-Your ouput code should satisfy these tests:
+4. Verify that your output code satisfies the following tests, but do not include the tests in the output code:
 {}
 '''
 
 
 complete_task_prompt_full = '''
-**Role**
-You are a Python software programmer.
-
-**Instructions**:
-1. As a programmer, you are required to complete the task described below.
-2. Make sure you understand the task and solve it using the correct Python syntax and proper indentation.
-3. Your output should be with no example usage and no test cases - stop generating immediately after the return statement or the final line of the function.
-4. Output only the complete, runnable Python code with no phrases like "Solution:" or "Here is a solution" and no other headings.
-5. Your output should be with no explanatory text and no other irrelevant content - it should be directly runnable by a Python interpreter without errors.
-
-**Code Format**
-Output your code in the following format:
+1. Act as an experienced Python software developer.
+2. Understand the task described below and generate an efficient Python solution that successfully passes all the tests listed below.
+3. Stop generating immediately after the return statement or the final line of the function.
+4. Exclude any non-code content. For example, exclude explanatory text, exclude example usage, exclude test cases, exclude phrases like "Solution:" or "Here is a solution", exclude any other headings, and exclude anything that can be considered as non-executable Python code.
+5. Your final output must be only code that can be executed directly by a Python interpreter without errors.
+6. Enclose your final output code in the following code fences:
 ```python
-<code>
+[code]
 ```
-
-**Task Description**
+7. Using the above instructions, complete the following task:
 {}
 
-Your ouput code should satisfy these tests:
+8. Verify that your output code satisfies the following tests, but do not include the tests in the output code:
 {}
 '''
 
+complete_task_prompt_full_OLD = '''
+1. Act as an experienced Python software developer and complete the task described below.
+2. Make sure you understand the task and generate a solution using Python's correct syntax and proper indentation.
+3. Your output must be only code and it must not contain any irrelevant non-code content: it should be with no explanatory text, no example usage, no test cases, no phrases like "Solution:" or "Here is a solution", and no other headings.
+4. Therefore, stop generating immediately after the return statement or the final line of the function.
+5. Your final output code must be directly runnable by a Python interpreter without errors.
+6. Enclose your final runnable output code in the following code fences:
+```python
+[code]
+```
+7. Using all of the above instructions, complete the following task:
+{}
+
+8. Verify that your output code satisfies the following tests, but do not include the tests in the output code:
+{}
+'''
+
+reflection_prompt_basic = '''1. Read and understand the REQUIREMENTS and the PROPOSED SOLUTION listed below.
+2. Correct and improve the PROPOSED SOLUTION.
+
+REQUIREMENTS:
+{}
+
+PROPOSED SOLUTION:
+{}'''
+
+reflection_prompt  = '''1. Carefully read and understand the REQUIREMENTS and the PROPOSED SOLUTION listed below.
+2. Correct any errors in the PROPOSED SOLUTION and improve it as much as possible.
+3. Enclose the new improved solution in the following code fences:
+```python
+[code]
+```
+
+REQUIREMENTS:
+{}
+
+PPROPOSED SOLUTION:
+{}'''
+
+reflection_prompt_full  = '''+++Refine(iterations=3)
+* Carefully read and understand the REQUIREMENTS and the PROPOSED SOLUTION listed below.
+* Correct all errors in the logic, syntax, or any other aspects of the PROPOSED SOLUTION. Remove any redundant code.
+* Make sure the new improved solution satisfied the REQUIREMENTS in the best possible way.
+
++++Output Criteria
+* Stop generating the new improved solution immediately after the return statement or the final line of the function.
+* Exclude any non-code content. For example, exclude explanatory text, exclude example usage, exclude test cases, exclude phrases like "Solution:" or "Here is a solution", exclude any other headings, and exclude anything that can be considered as non-executable Python code.
+* Your final output must be only code that can be executed by a Python interpreter without errors.
+
++++Output Format
+* Enclose the new improved solution in the following code fences:
+```python
+[code]
+```
+
++++REQUIREMENTS:
+{}
+
++++PROPOSED SOLUTION:
+{}'''
 
 
 if __name__ == "__main__":
-    print(human_eval_completion_prompt + 'Next line')
+    print(complete_code_prompt_basic.format('Next line'))
